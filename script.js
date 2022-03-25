2a3a6382834eff42dfda2036194f7f46ae691dd5
Thales Vanzo Vieira <thales.v.vieira@gmail.com>
2022-03-25 03:30:42 -0300

    troquei nome de algumas variaveis para que fique mais facil de ler, implementei uma funcao para retirar os acentos, porem temos que mostrar ao final da palavra completada a forma original (palavraRandom), limpei o campo de inserirLetra apos clicar no botao testar letra

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const temas = {
     cores: ["amarelo", "verde", "azul", "vermelho", "branco", "preto", "roxo", "cinza", "rosa", "marrom"]
 }
 
-let palavrarandom = ""; //palavra sorteada
-let arr = []; //array da palavra sorteada
-let text = ""; // letras erradas que aparecem na div "letraserradas"
-let letraescolhida = ""; //letra digitada pelo usuário para teste se tem na palavra
-let novoarray = []; //array da palavra substituído por "_" que mostra na tela
+let palavraRandom = ""; //palavra sorteada
+let palavraSorteadaSeparadaEmLetras = []; //array da palavra sorteada
+let letraErrada = ""; // letras erradas que aparecem na div "letraserradas"
+let letraEcolhida = ""; //letra digitada pelo usuário para teste se tem na palavra
+let arrayDeUnderlines = []; //array da palavra substituído por "_" que mostra na tela
+let palavraSemAcento = "";
 
 class Usuário{
     constructor(){
@@ -26,97 +27,101 @@ class Usuário{
     }
 }
 
-function sorteiapalavra(tema) { // funcão que sorteia a palavra, salva na "palavrarandom" e retorna a "palavrarandom"
+function sorteiapalavra(tema) { // funcão que sorteia a palavra, salva na "palavraRandom" e retorna a "palavraRandom"
 
     if(tema == "frutas") { 
-        palavrarandom =  temas.frutas[parseInt(Math.random() * temas.frutas.length)] // pega um elemento aleatório do array temas.frutas
+        palavraRandom =  temas.frutas[parseInt(Math.random() * temas.frutas.length)] // pega um elemento aleatório do array temas.frutas
     }
 
     else if (tema == "animais") { 
-        palavrarandom = temas.animais[parseInt(Math.random() * temas.animais.length)] // pega um elemento aleatório do array temas.animais
+        palavraRandom = temas.animais[parseInt(Math.random() * temas.animais.length)] // pega um elemento aleatório do array temas.animais
     }
 
     else if(tema == "cores") { 
-        palavrarandom = temas.cores[parseInt(Math.random() * temas.cores.length)] // pega um elemento aleatório do array temas.cores
+        palavraRandom = temas.cores[parseInt(Math.random() * temas.cores.length)] // pega um elemento aleatório do array temas.cores
     }
 
-    return palavrarandom;
+    return palavraRandom;
 }
 
-function mostranatela(palavra) { // recebe um array de "-" e transforma em string
-    let mostranatela = palavra.toString(); // o método tostring separa os elementos do array por ","
-        mostranatela = mostranatela.replace(/,/g, "");// esse replace troca todos as "," por espaços em branco, para ficar uma string de traços do mesmo tamanho que a palavra sorteada
-        console.log(mostranatela);
+function mostraNaTela(palavra) { // recebe um array de "-" e transforma em string
+    let mostraNaTela = palavra.toString(); // o método tostring separa os elementos do array por ","
+        mostraNaTela = mostraNaTela.replace(/,/g, "");// esse replace troca todos as "," por espaços em branco, para ficar uma string de traços do mesmo tamanho que a palavra sorteada
+        console.log(mostraNaTela);
 
-        document.getElementById("letras").innerHTML = mostranatela; //joga a string de traços na tela
+        document.getElementById("letras").innerHTML = mostraNaTela; //joga a string de traços na tela
 
 }
 
-// o arr é o array da palavra
-// o novo array é o array com os traços
+// o palavraSorteadaSeparadaEmLetras é o array da palavra separada letra a letra
+// o arrayDeUnderlines é o array com os traços
 
-function substituipalavrapor_(palavra) { 
-        arr = palavra.split(""); //pega a palavra e transforma num array de letras
-        console.log(arr);
+function subistituiPalavraPor_(palavra) { 
+        palavraSorteadaSeparadaEmLetras = palavra.split(""); //pega a palavra e transforma num array de letras
+        console.log(palavraSorteadaSeparadaEmLetras);
         
-        for(let i = 0; i<arr.length; i++) {
-            novoarray[i] = "_"; //laço para fazer um array de traços do mesmo tamanho que o da palavra
+        for(let i = 0; i<palavraSorteadaSeparadaEmLetras.length; i++) {
+            arrayDeUnderlines[i] = "_"; //laço para fazer um array de traços do mesmo tamanho que o da palavra
         }
 
-        console.log(novoarray);
+        console.log(arrayDeUnderlines);
         
-        mostranatela(novoarray); //chama a função mostra na tela passando o array de traços como parâmetro
+        mostraNaTela(arrayDeUnderlines); //chama a função mostra na tela passando o array de traços como parâmetro
 }
 
 function jogar() { //função chamada no click do botão "JOGAR"
     const usuario = new Usuário(); // CRIA UM NOVO USUÁRIO
-    palavrarandom = sorteiapalavra(usuario.temas); // SORTEIA A PALAVRA E SALVA
+    palavraRandom = sorteiapalavra(usuario.temas); // SORTEIA A PALAVRA E SALVA
     console.log(usuario);
-    console.log(palavrarandom);
-    substituipalavrapor_(palavrarandom);//chama a função que faz um array de traços do mesmo tamanho que a palavra passada como parâmetro
+    console.log(palavraRandom);
+    palavraSemAcento = palavraRandom.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); // tira todos os acentos, inclusive 'ç'
+    console.log(palavraSemAcento);
+    subistituiPalavraPor_(palavraSemAcento);//chama a função que faz um array de traços do mesmo tamanho que a palavra passada como parâmetro
 }
 
-function escreveletraerrada(letra) { //função que escreve as letras erradas que são recebidas como parâmetro na div "letraserradas"
-    text += `${letra} / `; // vai armazenando na variável text a letra errada separando por um /
-    document.getElementById("lj").innerHTML = text; // juga as letras erradas na tela
+function escreveLetraErrada(letra) { //função que escreve as letras erradas que são recebidas como parâmetro na div "letraserradas"
+    letraErrada += `${letra} / `; // vai armazenando na variável letraErrada a letra errada separando por um /
+    document.getElementById("lj").innerHTML = letraErrada; // juga as letras erradas na tela
 
-    console.log(text);
+    console.log(letraErrada);
 }
 
 function escreveletracerta(letra) { 
-    for(let i = 0; i<arr.length; i++) {
-        if(letra === arr[i]) {//esse teste compara a letra digitada, que é recebida como parâmetro, se existe no array da palavra
-            novoarray[i] = arr[i]; //aqui pega o array de traços e na mesma posição que a letra existe, substitui o traço pela letra 
+    for(let i = 0; i<palavraSorteadaSeparadaEmLetras.length; i++) {
+        if(letra === palavraSorteadaSeparadaEmLetras[i]) {//esse teste compara a letra digitada, que é recebida como parâmetro, se existe no array da palavra
+            arrayDeUnderlines[i] = palavraSorteadaSeparadaEmLetras[i]; //aqui pega o array de traços e na mesma posição que a letra existe, substitui o traço pela letra 
         }
     }
 
-    mostranatela(novoarray);//aqui chama a função mostra na tela passando o novoarray como parâmetro, esse novo array já foi modificado os traços pelas letras corretas
+    mostraNaTela(arrayDeUnderlines);//aqui chama a função mostra na tela passando o arrayDeUnderlines como parâmetro, esse novo array já foi modificado os traços pelas letras corretas
 }
 
 let erros = 0; //contador de erros, tantos erros é gameover
 
 function jogo() { // função chamada ao clicar no botão "testarletra"
-    letraescolhida = document.getElementById("letra").value; //guarda a letra digitada na variável letraescolhida
-    console.log(letraescolhida);
+    letraEcolhida = document.getElementById("letra").value; //guarda a letra digitada na variável letraEcolhida
+    document.getElementById("letra").value = "";
+    console.log(letraEcolhida);
 
-    let acertouletra = palavrarandom.includes(letraescolhida); //compara se a letra digital tem na palavra sorteada e armazena um true ou false na variável "acertouletra"
+    let acertouletra = palavraSemAcento.includes(letraEcolhida); //compara se a letra digitada tem na palavra sorteada e armazena um true ou false na variável "acertouletra"
     
 
     if(acertouletra) {//testa se acertou letra recebeu um true
-        escreveletracerta(letraescolhida); //chama a função "escreveletracerta" e passa a letra como parâmetro
+        escreveletracerta(letraEcolhida); //chama a função "escreveletracerta" e passa a letra como parâmetro
 
     } else { // se a letra digitada não existir na palavra
-        escreveletraerrada(letraescolhida); //chama a funcão "escreveletraerrada" passando a letra como parâmetro
+        escreveLetraErrada(letraEcolhida); //chama a funcão "escreveLetraErrada" passando a letra como parâmetro
         erros++; //incrementa o erros até o game over
         console.log("Erros " + erros)
     }
 }
 
+// FALTA FAZER
 
-// falta comparar o número de erros para gerar o gameover
-// falta a verificação de acentos e cedilha
-// para gerar vitória acredito que é só meter um novoarray.includes("_"), enquanto tiver devolvendo true continua o jogo, quando devolver um false é pq acertou todas as letras e dá a vitória
-
+// comparar o número de erros para gerar o gameover ou a mensagem de que acertou a palavra, mostrando com os acentos (palavraRandom)
+// para gerar vitória acredito que é só meter um arrayDeUnderlines.includes("_"), enquanto tiver devolvendo true continua o jogo, quando devolver um false é pq acertou todas as letras e dá a vitória
+// colocar opção de jogar novamente: mesmo login ou novo jogador? Caso seja um novo jogador, resetar o codigo do começo, caso seja o mesmo, limpar a tela e mostrar a nova palavraRandom (arrayDeUnderlines)
+// forçar que tenha um input de email valido
 
 
 
